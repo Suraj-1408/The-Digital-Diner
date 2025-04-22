6af8ff7d05dd6d1e66c81770a22532aba2616f4a
Suraj Manoj Paswan <surajpaswanc0497@gmail.com>
2025-04-22 09:44:07 +0530

    Initialization of Backend

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+const  express = require('express');
+const cors = require('cors');
+
+//importing db's
+const connectMongoDB = require('./db/mongo.js');
+const pool = require('./db/postgres.js');
+const menuRoutes = require('./routes/menuRoutes');
+const app = express();
+
+//(important for frontend to talk to backend)
+app.use(cors());
+
+
+//Middleware to parse JSON
+app.use(express.json());
+
+
+//creating index or home page
+app.get('/',(req,res) =>{
+    //console.log("Welcome to Digital Diner API");
+    res.send("Welcome to Digital Diner API");
+}); 
+
+//API route for menu items
+app.use('/api/menu', menuRoutes);
+
+
+//allocating port for it.
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port http://localhost:${PORT}/`);
+});

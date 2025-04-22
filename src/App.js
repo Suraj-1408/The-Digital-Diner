01c5cd811995e351baa5c6f40d551fb4e3bfdc44
Suraj Manoj Paswan <surajpaswanc0497@gmail.com>
2025-04-22 09:53:02 +0530

    MAin App that renders the application to frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
+
+import Menu from './components/Menu';
+import Cart from './components/Cart';
+
+import { CartProvider } from './context/CartContext';
+
 import './App.css';
 
 function App() {
   return (
+
+    <CartProvider>
+      <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <h1>Welcome to Digital Diner</h1>
+        <nav>
+        <Link to="/">Menu</Link> | <Link to="/cart">View Cart</Link>
+        </nav>
+
+
+        <Routes>
+          <Route path = "/" element ={<Menu/>}/>
+          <Route path = "/cart" element ={<Cart/>}/>
+          
+        </Routes>
     </div>
+    </Router>
+    </CartProvider>
   );
 }
 

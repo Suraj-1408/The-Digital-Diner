9be4c911c090967667909456a2c7d9c2ff65d918
Suraj Manoj Paswan <surajpaswanc0497@gmail.com>
2025-04-27 03:21:52 +0530

    printing mongo URL

--- Backend/db/mongo.js
+++ Backend/db/mongo.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();  // This ensures the environment variables are loaded
 
-const mongoURL = 'mongodb://localhost:27017/digitaldiner';
-
+//const mongoURL = 'mongodb://localhost:27017/digitaldiner';
+const mongoURL = process.env.MONGODB_URI;
+console.log('mongodb URL:',mongoURL);
 
     mongoose.connect(mongoURL)
 
